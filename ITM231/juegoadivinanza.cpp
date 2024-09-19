d9cad25e5472d28aeba1ef1a491f9ece6422eab2
Cristian Martinez Garcia <crisgicoba@gmail.com>
2024-09-19 11:32:52 -0400

    tercera version 19-09-2024

--- ITM231/juegoadivinanza.cpp
+++ ITM231/juegoadivinanza.cpp
@@ -8,5 +8,21 @@ cout <<"*********************************************************************"<<
 int numero_secreto = 42;
 cout <<"El numero secreto es ..." <<numero_secreto <<  " No lo diga a nadie! "<<endl;
 
+int adivina;
+cout << "¿ cual es el numero secreto?";
+cin >> adivina;
+cout <<"Elvalor de su numero es;  " << adivina  <<endl;
+
+if (adivina == numero_secreto){
+	cout <<"Elnumero ingresado es mayor que el numero secreto"<<endl;
+	}
+
+	else if(adivina > numero_secreto){
+	cout <<"el numero ingresado es mayor que el numero secreto"<<endl;
+	}
+
+	else{
+	cout <<"El numero ingresado es menor que el numero secreto"<<endl;
+	}
 }
 
