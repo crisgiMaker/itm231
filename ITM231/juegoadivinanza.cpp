9e4f1312aba1f1ee96991e7f138c4a90e8df10a1
Cristian Martinez Garcia <crisgicoba@gmail.com>
2024-10-01 09:39:18 -0400

    actualizacion 01-10-2024

--- ITM231/juegoadivinanza.cpp
+++ ITM231/juegoadivinanza.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-	cout <<"*********************************************************************"<<endl;
-	cout <<"*                Bienvenido al Juego de la Adivinanza               *"<<endl;
-	cout <<"*********************************************************************"<<endl;
+	cout <<"******************************************************************************"<<endl;
+	cout <<"*                    Bienvenido al Juego de la Adivinanza                    *"<<endl;
+	cout <<"*****************************************************************************"<<endl;
     cout <<endl;
 	cout <<endl;
     cout << "Ingrese un numero en el rango: 1 a 100*" << endl;
